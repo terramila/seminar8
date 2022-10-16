d9dfee907a815af79cd6e5ea7ae93cd7bf0b30c8
tarana <beshirova@hotmail.com>
2022-10-16 22:19:51 +0300

    task58

---
+++ Desktop/seminar8/task58/Program.cs
@@ -0,0 +1,84 @@
+﻿/*Задайте две матрицы.Напишите программу, которая будет находить произведение 
+двух матриц.Например, даны 2 матрицы:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+и
+1 5 8 5
+4 9 4 2
+7 2 2 6
+2 3 4 7
+Их произведение будет равно следующему массиву:
+1 20 56 10
+20 81 8 6
+56 8 4 24
+10 6 24 49 */
+Console.Write("Введите количество строк 1 массива: ");
+int rowsA = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов 1 массива: ");
+int columnsA = int.Parse(Console.ReadLine());
+Console.Write("Введите количество строк 2 массива: ");
+int rowsB = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов 2 массива: ");
+int columnsB = int.Parse(Console.ReadLine());
+if (columnsA != rowsB)
+{
+    Console.WriteLine("Такие матрицы умножать нельзя!");
+    return;
+}
+int[,] A = GetArray(rowsA, columnsA, 0, 10);
+int[,] B = GetArray(rowsB, columnsB, 0, 10);
+PrintArray(A);
+Console.WriteLine();
+PrintArray(B);
+Console.WriteLine();
+PrintArray(GetMultiplicationMatrix(A, B));
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetMultiplicationMatrix(int[,] arrayA, int[,] arrayB)
+{
+
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+
+
+
+
+            arrayC[i, j] = arrayA[i, j] * arrayB[i, j];
+
+        }
+    }
+
+
+    return arrayC;
+}
+
